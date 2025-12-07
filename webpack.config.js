dcd818f0f454b949646dbd3d50d76f246835c803
Alan Lobo <alansaviolobo@gmail.com>
2025-12-07 18:45:23 +0530

    Using webpack to minify all the html, js & css.

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const fs = require('fs');
+const CleanCSS = require('clean-css');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        main: './js/index.js',
+        'register-sw': './js/pwa/register-sw.js'
+    },
+    output: {
+        filename: 'js/[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // We use templateContent to modify the HTML before webpack processes it
+            templateContent: () => {
+                const html = fs.readFileSync('./index.html', 'utf8');
+                // Remove the original script tags to avoid duplication
+                return html
+                    .replace('<script defer type="module" src="js/index.js"></script>', '')
+                    .replace('<script defer src="js/pwa/register-sw.js"></script>', '');
+            },
+            filename: 'index.html',
+            chunks: ['main'], // Only inject main bundle automatically
+            scriptLoading: 'defer',
+            inject: 'head', // Inject into head as per original
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: 'css',
+                    to: 'css',
+                    transform(content, path) {
+                        if (path.endsWith('.css')) {
+                            return new CleanCSS({}).minify(content).styles;
+                        }
+                        return content;
+                    }
+                },
+                { from: 'assets', to: 'assets' },
+                { from: 'config', to: 'config' },
+                { from: 'data', to: 'data' },
+                { from: 'manifest.json', to: 'manifest.json' },
+                { from: 'offline.html', to: 'offline.html' },
+                { from: 'privacy.html', to: 'privacy.html' },
+                {
+                    from: 'js/pwa',
+                    to: 'js/pwa',
+                    globOptions: {
+                        ignore: ['**/register-sw.js'],
+                    },
+                },
+            ],
+        }),
+    ],
+};
